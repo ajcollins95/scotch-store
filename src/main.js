06a7275f31a18ecd6a46861659501a7afaa8f062
Aaron Collins <aa.collins.95@gmail.com>
2019-06-12 12:45:20 -0400

    Added admin and cart pages

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import router from './router';
 Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app'
   router,
-  template: '<App/>',
-  components: { App },
-});
+  render: h => h(App),
+}).$mount('#app');
